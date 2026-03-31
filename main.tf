e755147b27756a53da29a9d8777f93e83af9f975
Ibrahim Khan <ibikhan02@gmail.com>
2026-03-31 15:28:46 +0500

    Reverting

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 provider "aws" {
-  region     = "us-east-2"
+  region     = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
