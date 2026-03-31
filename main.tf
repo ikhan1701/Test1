2447f4746d37bcb1b2ed3e0292ee5faff26569f5
Ibrahim Khan <ibikhan02@gmail.com>
2026-03-31 15:17:05 +0500

    INital commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+# Create a simple resource (like a Virtual Private Cloud)
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
